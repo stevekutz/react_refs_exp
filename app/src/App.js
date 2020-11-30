c80d660f08bad1212b54bb0517bfc11d1480a6f7
stevekutz <stkutz@gmail.com>
2020-11-29 18:34:48 -0600

    added class based ref to create focus

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './App.css';
+import RefsDemoClass from './components/RefsDemoClass';
+
+// refs allow access of DOM nodes directly
+
+class App extends React.Component {
+
+
+
+    render () {
+        return (
+        
+
+            // 2) attach ref to element    
+            <div className  = "App">
+                <RefsDemoClass />
+            
+            </div>
+        
+        )
+    
+    }
+
 }
 
 export default App;
---
+++ app/src/components/RefsDemoClass.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+
+class RefsDemoClass extends Component {
+
+
+    constructor (props) {
+        super(props)
+        this.textAreaRef = React.createRef(); // 1) Define ref in constructor
+    
+    }
+
+
+
+    componentDidMount () {
+        
+        // this will display the current property which points to the textarea component
+        console.log(this.textAreaRef)
+
+        // 3) Call the focus method
+        this.textAreaRef.current.focus()
+    
+    }
+
+
+    render() {
+        return (
+            <div>    {/* 2) Attach ref to textarea element */}
+                <textarea type = "text"  ref = {this.textAreaRef}   placeholder = "we want focus here on page load"/>
+            
+            </div>
+
+
+
+        )
+    }
+}
+
+export default RefsDemoClass
