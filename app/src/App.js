7767779116724986875e49e236b7ffeb3249653d
stevekutz <stkutz@gmail.com>
2020-11-29 19:34:02 -0600

    setup useRef to count render cycles

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React , {useState, useEffect, useRef} from 'react';
+import './App.css';
+// import RefsDemoClass from './components/RefsDemoClass';
+// import RefsDemoClassLegacy from './components/RefsDemoClassLegacy';
+
+
+// refs allow access of DOM nodes directly
+// refs persist betwen renders, like state
+
+// refs DO NOT cause component to re-update when it changes
+// ref is just an obj with a current property
+
+
+export default function App () {
+
+    const[name, setName] = useState('')
+    const renderCount = useRef(1)
+
+    useEffect( () => {
+        renderCount.current = renderCount.current + 1    })
+
+
+    return (
+        <div>
+            <input value = {name} onChange = {e => setName(e.target.value)}/>
+            <p> Name is : {name}</p>
+            <div> render count : {renderCount.current} </div>
+        
+        
+        </div>
+    
+    )
+
 }
 
-export default App;
---
+++ app/src/components/RefsDemoClassLegacy.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class RefsDemoClassLegacy extends React.Component {
+    // older way to create refs
+    constructor (props){
+        super(props)
+        this.textAreaRefTwo = React.createRef();  // 1) Create ref
+        this.callbackRef = null;                 // 2) assign calback variable to null  & create callback function 
+
+        this.setCallBackRef = (elem) => {
+            this.callbackRef = elem
+        
+        }
+    
+    }
+
+
+    componentDidMount () {
+        if (this.callbackRef) {
+            console.log(this.textAreaRefTwo.current)
+            this.callbackRef.focus()
+        }
+        
+    
+
+    }
+
+    render() {
+    
+        return (
+            <div>
+                        {/* the textareea element is implicitly passed as a parameter to callbackRef  */}
+                <textarea ref = {this.setCallBackRef} type ="text" placeholder = "Older way to use refs to create focus" />
+            
+            </div>
+        )
+    }
+}
+
+export default RefsDemoClassLegacy;
